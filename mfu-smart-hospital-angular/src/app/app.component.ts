79a71556b388cd1bd13a2af4257775469efe9071
LuRyanXdi <minswanpyae7@gmail.com>
2024-10-30 14:41:42 +0700

    footer

--- mfu-smart-hospital-angular/src/app/app.component.ts
+++ mfu-smart-hospital-angular/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from '../pages/login/login.component';
 import { PatientQueueComponent } from '../pages/patient-queue/patient-queue.component';
 import { HeaderComponent } from "../pages/header/header.component";
+import { FooterComponent } from "../pages/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, PatientQueueComponent, HeaderComponent],
+  imports: [RouterOutlet, LoginComponent, PatientQueueComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
