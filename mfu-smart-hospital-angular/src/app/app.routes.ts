a0db4f3955dcc0115bae31c33d7c54fe17f4284a
LuRyanXdi <minswanpyae7@gmail.com>
2024-10-30 13:49:50 +0700

    header

--- mfu-smart-hospital-angular/src/app/app.routes.ts
+++ mfu-smart-hospital-angular/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from '../pages/login/login.component';
 import { PatientQueueComponent } from '../pages/patient-queue/patient-queue.component';
+//import { HomeComponent } from '../pages/';
+//import { Service } from '../pages/';
+//import { PatientRecord } from '../pages/';
+//import { History } from '../pages/';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
