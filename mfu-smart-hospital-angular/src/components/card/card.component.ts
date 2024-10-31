64909f3db0b76a2362e067ba1450574f73e0af93
ThetSweLin <6531503189@lamduan.mfu.ac.th>
2024-10-31 16:47:58 +0700

    centers page

---
+++ mfu-smart-hospital-angular/src/components/card/card.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent {
+  @Input() name: string = "";
+  @Input() imageUrl: string = "";
+}
