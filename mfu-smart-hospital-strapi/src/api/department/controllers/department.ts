47cc82ab33e3300c618aba309554df73cec84a7b
ThetSweLin <6531503189@lamduan.mfu.ac.th>
2024-11-01 22:04:22 +0700

    updated cenetrs&admin-centers

--- mfu-smart-hospital-strapi/src/api/department/controllers/department.ts
+++ mfu-smart-hospital-strapi/src/api/department/controllers/department.ts
@@ -1,7 +1,15 @@
 /**
  * department controller
  */
+import { factories } from '@strapi/strapi';
 
-import { factories } from '@strapi/strapi'
+const departmentController = factories.createCoreController('api::department.department', ({ strapi }) => ({
+  // Method to find all departments
+  async find(ctx) {
+    const { query } = ctx;
+    const departments = await strapi.service('api::department.department').find(query);
+    return departments;
+  },
+}));
 
 export default factories.createCoreController('api::department.department');
