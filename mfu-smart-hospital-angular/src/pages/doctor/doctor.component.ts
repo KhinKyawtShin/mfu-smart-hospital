cda32261e21d8c534a4a08dd328c2a8f279a9e22
LuRyanXdi <minswanpyae7@gmail.com>
2024-11-05 12:44:33 +0700

    Saranghae

--- mfu-smart-hospital-angular/src/pages/doctor/doctor.component.ts
+++ mfu-smart-hospital-angular/src/pages/doctor/doctor.component.ts
@@ -3,17 +3,19 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from "../header/header.component";
 import { FooterComponent } from "../footer/footer.component";
+import { CardComponent } from "../../components/card/card.component";
 
 
 @Component({
  selector: 'app-doctor',
  standalone: true,
- imports: [CommonModule, HttpClientModule, HeaderComponent, FooterComponent],
+ imports: [CommonModule, HttpClientModule, HeaderComponent, FooterComponent, CardComponent],
  templateUrl: './doctor.component.html',
  styleUrl: './doctor.component.css'
 })
 export class DoctorComponent implements OnInit {
  doctors: any[] = [];
+ filteredDoctors: any[] = []; // Store filtered doctors
 
 
  constructor(private http:HttpClient) {}
@@ -25,17 +27,29 @@ export class DoctorComponent implements OnInit {
 
 
  fetchDoctorData(): void {
-   this.http.get<any>('http://localhost:1337/api/doctors').subscribe({
+   this.http.get<any>('http://localhost:1337/api/doctors?populate=*').subscribe({
      next: (response) => {
-       console.log('API Response:', response);
-       this.doctors = response.data;
-     },
-     error: (err) => {
-       console.error('Error fetching doctors:', err);
-     } 
+      console.log('API Response:', response);
+      if (response && response.data && response.data.length > 0){
+        this.doctors = response.data.map((doctor: any)=> ({
+          name: doctor.name,
+          department: doctor.department?.name
+        }));
+        this.filteredDoctors = this.doctors; // 
+      } else {
+        console.warn('No doctors available in API response.');
+      }
+    },
+    error: (err) => {
+      console.error('Error fetching centers data:', err);
+    }
     
    })
  }
+
+ filterDoctorsByDepartment(department: string): void {
+  this.filteredDoctors = this.doctors.filter(doctor => doctor.department === department);
+}
 
 
  goNext(): void {
