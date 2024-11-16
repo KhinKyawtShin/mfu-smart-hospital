0f48f24a10d30593443be05661e7e0312ab53465
ThetSweLin <6531503189@lamduan.mfu.ac.th>
2024-11-16 22:52:15 +0700

    header username

--- mfu-smart-hospital-angular/src/pages/header/header.component.ts
+++ mfu-smart-hospital-angular/src/pages/header/header.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
+  providers: [UserService],
   imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  userName : string = '';
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    // Retrieve the username from UserService
+    const currentUser = this.userService.getCurrentUser();
+    if (currentUser) {
+      console.log('Logged in as:', currentUser.username);
+      this.userName = currentUser.username;
+    } else {
+      console.warn('No user is logged in.');
+      this.userName = 'null';
+    }
+}
+
   currentLanguage: string = 'EN';
   isDropdownOpen: boolean = false;
 
